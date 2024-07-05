c85dbb7de8a578c07add7337e0ce30c5a779a4dc
nareshyadav45 <131270461+nareshyadav45@users.noreply.github.com>
2024-07-05 11:38:06 +0530

    Update EnquiryStudentController.java

--- JRTP_Office_Team_Portal_SignUp_Student_Enquiry_Management_Mini_02/src/main/java/com/enquiry/student/controllers/EnquiryStudentController.java
+++ JRTP_Office_Team_Portal_SignUp_Student_Enquiry_Management_Mini_02/src/main/java/com/enquiry/student/controllers/EnquiryStudentController.java
@@ -1,7 +1,6 @@
 package com.enquiry.student.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.enquiry.student.binding.DashboardUser;
 import com.enquiry.student.binding.EnquiryFilterForm;
 import com.enquiry.student.binding.EnquiryForm;
